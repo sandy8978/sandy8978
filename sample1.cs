3a4d8d76f63a251499eb7d016409395d0e3b7e7b
sandeep <142823664+sandy8978@users.noreply.github.com>
2024-02-04 19:43:40 +0530

    Add files via upload

---
+++ sample1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class sample1
+    {
+        public static void Main()
+        {
+            Console.Write("Enter Eno: ");
+            string Eno = Console.ReadLine();
+            Console.Write("Enter Ename: ");
+            string Ename = Console.ReadLine();
+            Console.Write("Enter Salary: ");
+            string Salary = Console.ReadLine(); 
+            Console.Write("Enter Increment: ");
+            string Inc = Console.ReadLine();    
+            Console.WriteLine("Total Salary = " + (Salary + (Inc)));
+            string tot = Console.ReadLine();
+            Console.WriteLine("Employee No is: " + Eno + ", Employee Name is: " + Ename + " and Salary is: " + Salary);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ sample2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications
+{
+    class sample2
+    {
+        public static void Main(string[] args)
+        {
+             //impliclit tye casting//
+            //int x = 40;
+           // double y = x;
+            
+            //exclipt type casting//
+            double x = 40.43;
+            int y = (int)x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+        }
+    }
+}
